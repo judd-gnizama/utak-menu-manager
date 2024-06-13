d99747e4f2e9e4a8d7a5f554c74a1b92f0d35c26
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-06-14 00:38:52 +0800

    Added Routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
