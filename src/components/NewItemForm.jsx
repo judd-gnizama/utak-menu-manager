0f37d3dc8aff8dbc1230b010b094e860e0dc6d9e
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-06-15 09:52:58 +0800

    changes to New item

--- src/components/NewItemForm.jsx
+++ src/components/NewItemForm.jsx
@@ -11,8 +11,55 @@ const NewItemForm = ({ show, setShow }) => {
     stock: "",
     available: true,
     description: "",
-    category: "Unassigned",
-    var_options: [],
+    category: "",
+    var_options: [
+      {
+        var_group: "Size",
+        variants: [
+          {
+            var_name: "Small",
+            priceDelta: 0,
+            costDelta: 0,
+            available: true,
+          },
+          {
+            var_name: "Medium",
+            priceDelta: 500,
+            costDelta: -200,
+            available: true,
+          },
+          {
+            var_name: "Large",
+            priceDelta: -800,
+            costDelta: 300,
+            available: true,
+          },
+        ],
+      },
+      {
+        var_group: "Toppings",
+        variants: [
+          {
+            var_name: "Pepperoni",
+            priceDelta: 120,
+            costDelta: 20,
+            available: true,
+          },
+          {
+            var_name: "Ham",
+            priceDelta: 300,
+            costDelta: 100,
+            available: true,
+          },
+          {
+            var_name: "Cheese",
+            priceDelta: 400,
+            costDelta: 200,
+            available: true,
+          },
+        ],
+      },
+    ],
   });
 
   const [categoryOptions, setCategoryOptions] = useState([]);
@@ -23,31 +70,49 @@ const NewItemForm = ({ show, setShow }) => {
     console.log(await getAllCategories());
   };
 
+  // const handleAddVariation = () => {
+  //   if (variation) {
+  //     const existVariation = itemFormData.var_options?.filter(
+  //       (opt) => opt.name === variation
+  //     );
+  //     console.log(existVariation);
+  //     if (existVariation.length <= 0)
+  //       setItemFormData({
+  //         ...itemFormData,
+  //         var_options: [
+  //           ...itemFormData.var_options,
+  //           {
+  //             name: variation,
+  //             variants: [
+  //               {
+  //                 variantName: "",
+  //                 variantPrice: "",
+  //                 variantCost: "",
+  //               },
+  //             ],
+  //           },
+  //         ],
+  //       });
+  //   }
+  //   console.log(itemFormData);
+  // };
+
   const handleAddVariation = () => {
-    if (variation) {
-      const existVariation = itemFormData.var_options?.filter(
-        (opt) => opt.name === variation
-      );
-      console.log(existVariation);
-      if (existVariation.length <= 0)
-        setItemFormData({
-          ...itemFormData,
-          var_options: [
-            ...itemFormData.var_options,
-            {
-              name: variation,
-              variants: [
-                {
-                  variantName: "",
-                  variantPrice: "",
-                  variantCost: "",
-                },
-              ],
-            },
-          ],
-        });
-    }
-    console.log(itemFormData);
+    const newVariation = {
+      var_group: "",
+      variants: [
+        {
+          var_name: "",
+          priceDelta: "",
+          costDelta: "",
+          available: true,
+        },
+      ],
+    };
+    setItemFormData({
+      ...itemFormData,
+      var_options: [...itemFormData.var_options, newVariation],
+    });
   };
 
   const handleCreateCategory = async () => {
@@ -162,7 +227,7 @@ const NewItemForm = ({ show, setShow }) => {
             </label>
             <span>Required</span>
           </span>
-          <select
+          {/* <select
             name="item-category"
             id="item-category"
             className="p-2 border rounded-md w-full"
@@ -178,8 +243,29 @@ const NewItemForm = ({ show, setShow }) => {
                   </option>
                 ))
               : {}}
-          </select>
-          <p className="text-[0.75rem] ml-1">
+          </select> */}
+          <input
+            name="item-category"
+            id="item-category"
+            type="text"
+            list="category-list"
+            value={itemFormData.category}
+            onChange={(e) =>
+              setItemFormData({ ...itemFormData, category: e.target.value })
+            }
+            placeholder="Create or Search Category"
+            className="p-2 border rounded-md w-full"
+          />
+          <datalist id="category-list">
+            {categoryOptions
+              ? categoryOptions.map((opt, index) => (
+                  <option key={index} value={opt}>
+                    {opt}
+                  </option>
+                ))
+              : {}}
+          </datalist>
+          {/* <p className="text-[0.75rem] ml-1">
             Missing a category?{" "}
             <a
               onClick={handleCreateCategory}
@@ -187,7 +273,16 @@ const NewItemForm = ({ show, setShow }) => {
             >
               Add category
             </a>
-          </p>
+          </p> */}
+          {/* <p className="text-[0.75rem] ml-1">
+            Missing a category?{" "}
+            <a
+              onClick={handleCreateCategory}
+              className="font-bold text-[var(--primary-color)] cursor-pointer"
+            >
+              Add category
+            </a>
+          </p> */}
         </div>
         <div className="grid gap-2">
           <span className="flex justify-between">
@@ -196,7 +291,7 @@ const NewItemForm = ({ show, setShow }) => {
             </label>
             <span>Optional</span>
           </span>
-          <ul className="">
+          {/* <ul className="">
             {itemFormData.var_options
               ? itemFormData.var_options.map((variant, index) => (
                   <li key={index} className="grid grid-cols-[2fr_1fr_1fr_1fr]">
@@ -223,33 +318,186 @@ const NewItemForm = ({ show, setShow }) => {
                   </li>
                 ))
               : {}}
-          </ul>
-          <div className="grid grid-cols-[2fr_1fr_1fr_1fr] gap-4">
-            <div className="flex items-center gap-2 col-span-2">
-              <label htmlFor="var-group-name">Option Name</label>
-              <input
-                name="var-group-name"
-                id="var-group-name"
-                type="text"
-                placeholder="E.g. Size"
-                value={variation}
-                onChange={(e) => setVariation(e.target.value)}
-                className="p-2 border rounded-md w-full"
-              />
-              <button
-                type="button"
-                onClick={handleAddVariation}
-                className="button-2 rounded-[3px]"
-              >
-                Add
-              </button>
-            </div>
+          </ul> */}
+          <div className="grid gap-4">
+            {itemFormData.var_options
+              ? itemFormData.var_options.map((_variation, index) => (
+                  <div
+                    key={index}
+                    className="grid grid-cols-4 gap-2 border rounded-md p-3 relative"
+                  >
+                    <button
+                      type="button"
+                      className="material-symbols-outlined filled text-gray-300 p-2 absolute right-0"
+                    >
+                      close
+                    </button>
+                    <div className="flex items-center gap-2 col-span-2">
+                      <label htmlFor="var-group-name" className="text-nowrap">
+                        Variation Name
+                      </label>
+                      <input
+                        name="var-group-name"
+                        id="var-group-name"
+                        type="text"
+                        placeholder="E.g. Size"
+                        value={_variation.var_group}
+                        className="p-2 border rounded-md w-full"
+                      />
+                    </div>
+                    {_variation.variants.map((_variant, index) => (
+                      <div
+                        key={index}
+                        className="flex gap-3 col-start-1 ml-8 col-span-full "
+                      >
+                        <div className="flex items-center gap-1">
+                          <input
+                            name={"variant-name" + _variant.var_name}
+                            id={"variant-name" + _variant.var_name}
+                            placeholder={"Variant " + (index + 1)}
+                            value={_variant.var_name}
+                            className="p-2 border rounded-md w-full"
+                          />
+                        </div>
+                        <div className="flex items-center gap-1">
+                          <label
+                            htmlFor={"variant-priceAdj" + _variant.var_name}
+                            className="text-nowrap"
+                          >
+                            Price Adj
+                          </label>
+                          <input
+                            name={"variant-priceAdj" + _variant.var_name}
+                            id={"variant-priceAdj" + _variant.var_name}
+                            type="number"
+                            min="0"
+                            value={_variant.priceDelta}
+                            className="p-2 border rounded-md w-full"
+                          />
+                        </div>
+                        <div className="flex items-center gap-1">
+                          <label
+                            htmlFor={"variant-costAdj" + _variant.var_name}
+                            className="text-nowrap"
+                          >
+                            Cost Adj
+                          </label>
+                          <input
+                            name={"variant-costAdj" + _variant.var_name}
+                            id={"variant-costAdj" + _variant.var_name}
+                            type="number"
+                            min="0"
+                            value={_variant.costDelta}
+                            className="p-2 border rounded-md w-full"
+                          />
+                        </div>
+
+                        <label
+                          htmlFor={"variant-availability" + _variant.var_name}
+                          className="inline-flex items-center gap-1 font-bold"
+                        >
+                          <input
+                            type="checkbox"
+                            name={"variant-availability" + _variant.var_name}
+                            id={"variant-availability" + _variant.var_name}
+                            checked={_variant.available}
+                          />
+                          Available
+                        </label>
+                        <button
+                          type="button"
+                          className="material-symbols-outlined filled text-gray-300 p-2"
+                        >
+                          close
+                        </button>
+                      </div>
+                    ))}
+                    {/* <div className="flex gap-3 col-start-1 ml-8 col-span-full ">
+                      <div className="flex items-center gap-1">
+                        <input
+                          name="item-cost"
+                          id="item-cost"
+                          placeholder="Variant 2"
+                          className="p-2 border rounded-md w-full"
+                        />
+                      </div>
+                      <div className="flex items-center gap-1">
+                        <label htmlFor="item-stock" className="text-nowrap">
+                          Price Adj
+                        </label>
+                        <input
+                          name="item-stock"
+                          id="item-stock"
+                          type="number"
+                          min="0"
+                          value={itemFormData.stock}
+                          onChange={(e) =>
+                            setItemFormData({
+                              ...itemFormData,
+                              stock: e.target.value,
+                            })
+                          }
+                          className="p-2 border rounded-md w-full"
+                        />
+                      </div>
+                      <div className="flex items-center gap-1">
+                        <label htmlFor="item-stock" className="text-nowrap">
+                          Cost Adj
+                        </label>
+                        <input
+                          name="item-stock"
+                          id="item-stock"
+                          type="number"
+                          min="0"
+                          value={itemFormData.stock}
+                          onChange={(e) =>
+                            setItemFormData({
+                              ...itemFormData,
+                              stock: e.target.value,
+                            })
+                          }
+                          className="p-2 border rounded-md w-full"
+                        />
+                      </div>
+                      <label
+                        htmlFor="availability"
+                        className="inline-flex items-center gap-2 font-bold"
+                      >
+                        <input
+                          type="checkbox"
+                          name="availability"
+                          id="availability"
+                          className="ml-2 scale-110"
+                        />
+                        Available
+                      </label>
+                      <button
+                        type="button"
+                        className="material-symbols-outlined filled text-gray-300 p-2"
+                      >
+                        delete
+                      </button>
+                    </div> */}
+                    <button
+                      type="button"
+                      className="inline-flex items-center justify-center border rounded-md ml-8 col-span-full"
+                    >
+                      <span className="material-symbols-outlined">
+                        keyboard_return
+                      </span>
+                      Add New Variant
+                    </button>
+                  </div>
+                ))
+              : {}}
+
             <button
               type="button"
-              className="col-span-full inline-flex items-center justify-center border rounded-md"
+              className="inline-flex items-center justify-center border rounded-md"
+              onClick={handleAddVariation}
             >
               <span className="material-symbols-outlined">add</span>Add New
-              Variant
+              Variation
             </button>
           </div>
         </div>
